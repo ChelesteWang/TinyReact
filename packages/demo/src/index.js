14c82d8495aa4fa0197a240cdc04e455baf97dc8
ChelesteWang <1581446178@qq.com>
2022-02-27 19:13:56 +0800

    feat: 🎸 更新 demo 注释

--- packages/demo/src/index.js
+++ packages/demo/src/index.js
@@ -1,11 +1,14 @@
 import React from "@tinyreact/react";
 import ReactDOM from "@tinyreact/react-dom";
 
+// 使用 React 和 ReactDOM 创建一个简单的组件
+
+// 用于 babel 转换的 jsx 语法
 /** @jsx React.h */
 /** @jsxFrag React.Fragment */
 
 /**
- * React Component
+ * 创建一个 React 组件
  * @returns {React.Component}
  */
 export default function App() {
@@ -17,7 +20,7 @@ export default function App() {
 }
 
 /**
- * Render the component
+ * 渲染 React 组件
  * @param {React.Component} App
  * @param {HTMLElement} document.getElementById("app")
  */
