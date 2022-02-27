86bda562552c88b65cb7f8426fcf2fd9fc34ce59
ChelesteWang <1581446178@qq.com>
2022-02-27 19:04:30 +0800

    feat: 🎸 添加 webpack 以及 babel 用于 Demo 测试

--- packages/demo/src/index.js
+++ packages/demo/src/index.js
@@ -1,6 +1,13 @@
 import React from "@tinyreact/react";
 import ReactDOM from "@tinyreact/react-dom";
 
+/** @jsx React.h */
+/** @jsxFrag React.Fragment */
+
+/**
+ * React Component
+ * @returns {React.Component}
+ */
 export default function App() {
   return (
     <div>
@@ -9,4 +16,9 @@ export default function App() {
   );
 }
 
+/**
+ * Render the component
+ * @param {React.Component} App
+ * @param {HTMLElement} document.getElementById("app")
+ */
 ReactDOM.render(<App />, document.getElementById("app"));
