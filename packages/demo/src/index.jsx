f96dbc9a98434d76e7fb753cdc6e4c0597724170
ChelesteWang <1581446178@qq.com>
2022-02-27 18:29:09 +0800

    feat: 🎸 添加 TinyReact demo

---
+++ packages/demo/src/index.jsx
@@ -0,0 +1,12 @@
+import React from "@tinyreact/react";
+import ReactDOM from "@tinyreact/react-dom";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
